c5839238f1c3bc03bf7eb3fe56a6e7e724ea89a8
casperlamboo <casperlamboo@gmail.com>
2016-04-18 09:30:07 +0200

    start queue

--- src/restapi.js
+++ src/restapi.js
@@ -3,6 +3,10 @@ import $ from 'jquery';
 const GET_TIMEOUT = 5000;
 const POST_TIMEOUT = 10000;
 
+// TODO
+// implement queue meganism
+const queue = [];
+
 export function get (url) {
 	return new Promise((resolve, reject) => {
 		$.ajax({
