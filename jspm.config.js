a7bb60f4615f3a01cbb5d0b5f6d50b614f0676b6
casperlamboo <casperlamboo@gmail.com>
2016-07-19 11:16:34 +0200

    updated to jspm 0.17

---
+++ jspm.config.js
@@ -0,0 +1,51 @@
+SystemJS.config({
+  paths: {
+    "github:": "jspm_packages/github/",
+    "npm:": "jspm_packages/npm/",
+    "Doodle3D-API/": "src/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "babel-runtime": "npm:babel-runtime@5.8.38",
+      "core-js": "npm:core-js@1.2.7",
+      "traceur": "github:jmcriffey/bower-traceur@0.0.90",
+      "traceur-runtime": "github:jmcriffey/bower-traceur-runtime@0.0.90",
+      "process": "github:jspm/nodelibs-process@0.2.0-alpha",
+      "fs": "github:jspm/nodelibs-fs@0.2.0-alpha",
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.12",
+      "path": "github:jspm/nodelibs-path@0.2.0-alpha"
+    }
+  },
+  transpiler: "plugin-babel",
+  babelOptions: {
+    "optional": [
+      "runtime"
+    ],
+    "stage": 0
+  },
+  map: {
+    "babel": "npm:babel-core@5.7.4"
+  },
+  packages: {
+    "Doodle3D-API": {
+      "main": "index.js"
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json",
+    "github:*/*.json"
+  ],
+  map: {
+    "casperlamboo/EventDispatcher": "github:casperlamboo/EventDispatcher@master",
+    "github/fetch": "github:github/fetch@0.9.0",
+    "jquery": "github:components/jquery@2.2.4"
+  },
+  packages: {}
+});
